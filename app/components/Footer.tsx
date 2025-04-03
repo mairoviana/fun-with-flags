65bac3b81def0441097366024da5bbc4a8b8777c
Maíro Viana <mairo_viana@hotmail.com>
2025-04-03 20:29:47 -0300

    Add footer and header componets

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-6 mb-8">
+      <p className="text-center">Made with ♥️ by Maíro Viana</p>
+    </footer>
+  );
+};
+
+export default Footer;
